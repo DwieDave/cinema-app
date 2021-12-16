226f6ef643ee3a691e8f7b7c7dd9ad0bcc5a0fd3
David Gruber <david@gruber.tv>
2021-12-16 14:27:14 +0100

    optimized build scripts

--- build.sh
+++ build.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 npm run debug
+lessc -clean-css ./webapp/src/styles/style.less ./webapp/build/styles/style.css 
 terser ./webapp/build/js/main.js --compress --mangle -o ./webapp/build/js/main.js
--- debug.sh
+++ debug.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
-cp ./webapp/src/index.html ./webapp/build/index.html 
+rm -rf ./webapp/build
+mkdir ./webapp/build
+cp ./webapp/src/index.html ./webapp/build/index.html
 cp -r ./webapp/src/img ./webapp/build/ 
-lessc -clean-css ./webapp/src/styles/style.less ./webapp/build/styles/style.css 
+lessc ./webapp/src/styles/style.less ./webapp/build/styles/style.css 
 browserify ./webapp/src/js/Main.js ./node_modules/uikit/dist/js/uikit.js ./node_modules/uikit/dist/js/uikit-icons.js -o ./webapp/build/js/main.js
