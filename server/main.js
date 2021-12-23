1a5a8ff0aeb86b08f12956175813808046024747
David Gruber <david@gruber.tv>
2021-12-23 14:39:20 +0100

    added mongoose and db models

--- server/main.js
+++ server/main.js
@@ -2,10 +2,34 @@ const express = require('express');
 const app = express();
 const port = process.argv[2];
 const path = require('path');
+const mongoose = require('mongoose');
+const { Cinema, Presentation, Reservation } = require('./models');
+
+async function connectDB() {
+  await mongoose.connect('mongodb://localhost:27017/cinema-app');
+}
+connectDB().catch(err => console.log(err));
 
 app.use(express.static(path.join(__dirname, '../', 'webapp/build')));
 
-app.get('/debug', function (req, res) {
+app.get('/newCinema', (req, res) => {
+  const newCinema = new Cinema({ name: "Kinosaal 2", seatRows: 15, seatsPerRow: 30 });
+  newCinema.save();
+
+  const newPresentation = new Presentation({ cinema: newCinema._id, date: Date.now(), movieTitle: "Avengers: Endgame" });
+  newPresentation.save();
+
+  const newReservation = new Reservation({ presentation: newPresentation._id, reservedSeats: 2, customerName: "John Doe" });
+  newReservation.save();
+  res.send("success");
+});
+
+app.get('/presentation', async (req, res) => {
+  const presentation = await Presentation.find().populate('cinema', 'name').exec();
+  res.json(presentation);
+});
+
+app.get('/debug', (req, res) => {
   res.send(JSON.stringify(process.argv));
 });
 
---
+++ server/models.js
@@ -0,0 +1,27 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const cinemaSchema = Schema({
+    name: String,
+    seatRows: Number,
+    seatsPerRow: Number
+});
+const Cinema = mongoose.model("Cinema", cinemaSchema);
+
+
+const presentationSchema = Schema({
+    cinema: { type: Schema.Types.ObjectId, ref: 'Cinema' },
+    date: Date,
+    movieTitle: String
+});
+const Presentation = mongoose.model("Presentation", presentationSchema);
+
+
+const reservationSchema = Schema({
+    presentation: { type: Schema.Types.ObjectId, ref: 'Presentation' },
+    reservedSeats: Number,
+    customerName: String
+});
+const Reservation = mongoose.model("Reservation", reservationSchema);
+
+module.exports = { Cinema, Presentation, Reservation };
