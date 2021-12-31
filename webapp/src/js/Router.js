b2ff54e439ff38cc5c5120ae0134efd37c04edc0
David Gruber <david@gruber.tv>
2021-12-31 18:46:24 +0100

    renamed initialize to createPage

--- webapp/src/js/Router.js
+++ webapp/src/js/Router.js
@@ -13,12 +13,17 @@ module.exports = class Router {
       { path: '/page/presentations', viewClass: PresentationsPage }
     ];
 
-    this.initialize();
+    this.createPage();
   }
 
-  async initialize () {
-    this.page = this.findPage(window.location.pathname);
-    if (this.page) await this.renderPage(this.page);
+  async createPage () {
+    const page = this.findPage(window.location.pathname);
+    if (page) {
+      // creating class
+      const PageClass = (page.viewClass);
+      this.page = new PageClass();
+      await this.renderPage(page);
+    }
     // TODO: else: render Error Page
   }
 
@@ -35,8 +40,8 @@ module.exports = class Router {
   }
 
   /*  renderPage: Creates a Page Class according to the viewClass property of the RouterPage
-    Renders HTML content of class and registers clickHandlers of the class if given
-    removes/adds ui-kits active class to the menu-entry linking to the current page */
+        Renders HTML content of class and registers clickHandlers of the class if given
+        removes/adds ui-kits active class to the menu-entry linking to the current page */
 
   async renderPage (routerPage) {
     // setting 'uk-active' class if current path is linked in the menu-item
@@ -49,15 +54,11 @@ module.exports = class Router {
       if (child.children[0].attributes.href.value === routerPage.path) child.classList.add('uk-active');
     }
 
-    // creating class
-    const PageClass = (routerPage.viewClass);
-    const page = new PageClass();
-
     // rendering HTML content into #app container
     const appContainer = document.querySelector('#app');
     if (appContainer) {
       appContainer.innerHTML = '';
-      appContainer.innerHTML = await page.render();
+      appContainer.innerHTML = await this.page.render();
 
       // Workaround to re-trigger uikit animation on pageRender
       appContainer.style.animation = 'none';
@@ -65,22 +66,22 @@ module.exports = class Router {
         return element.offsetHeight;
       })(appContainer);
       appContainer.style.animation = null;
-    }
 
-    // registering pages clickHandlers - if given
-    const clickHandlers = page.clickHandler;
-    if (clickHandlers?.length > 0) {
-      for (const clickHandler of clickHandlers) {
-        page.registerClickHandler(clickHandler.querySelector, clickHandler.callback);
+      // registering pages clickHandlers - if given
+      const clickHandlers = this.page.clickHandler;
+      if (clickHandlers?.length > 0) {
+        for (const clickHandler of clickHandlers) {
+          this.page.registerClickHandler(clickHandler.querySelector, clickHandler.callback);
+        }
       }
     }
   }
 
   /*  navigateTo: change window url without navigating to it
-    call pageRouter function to render content based on the new url */
+        call pageRouter function to render content based on the new url */
 
   navigateTo (url) {
     window.history.pushState(null, null, url);
-    this.initialize();
+    this.createPage();
   }
 };
