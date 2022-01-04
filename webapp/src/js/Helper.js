f9d920f1d537712f63e93f0f6e0ad7d1db553489
David Gruber <david@gruber.tv>
2022-01-04 21:09:13 +0100

    helper functions

--- webapp/src/js/Helper.js
+++
@@ -1,6 +0,0 @@
-function formatDate (dateString) {
-  const date = new Date(dateString);
-  const options = { year: 'numeric', month: '2-digit', day: 'numeric', hour: '2-digit', minute: '2-digit' };
-  return date.toLocaleDateString('de-DE', options);
-}
-module.exports = { formatDate };
