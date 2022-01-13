c73aa4d6306866746a41b2c253fff847226d7de0
David Gruber <david@gruber.tv>
2022-01-13 10:32:26 +0100

    re-structured & commented AbstractPage functions

--- webapp/src/js/pages/AbstractPage.js
+++ webapp/src/js/pages/AbstractPage.js
@@ -5,7 +5,7 @@ const UIkit = require('uikit');
 /*  AbstractPage: Parent-Class for all pages.
     Defines function names and offers common functions */
 module.exports = class AbstractPage {
-  constructor (params) {
+  constructor(params) {
     this.apiURL = 'http://localhost:8080/v1';
     this.params = params;
     this.formid = '';
@@ -24,17 +24,22 @@ module.exports = class AbstractPage {
       gt: (v1, v2) => v1 > v2,
       lte: (v1, v2) => v1 <= v2,
       gte: (v1, v2) => v1 >= v2,
-      and () {
+      and() {
         return Array.prototype.every.call(arguments, Boolean);
       },
-      or () {
+      or() {
         return Array.prototype.slice.call(arguments, 0, -1).some(Boolean);
       }
     });
   }
 
-  // Pagination
-  addPaginationHandler () {
+  // ----------------------------------------------
+
+  /* 
+   * Pagination functions 
+   */
+
+  addPaginationHandler() {
     if (!this.isValid(this.clickHandler)) this.clickHandler = [];
     this.clickHandler.push({
       querySelector: '.changeToPage, .nextPage, .previousPage',
@@ -45,7 +50,9 @@ module.exports = class AbstractPage {
     });
   }
 
-  addPaginationListener () {
+  /* addPaginationListener: adds Listeners to the eventListeners 
+     Array necessary for calculating the amount of elements per page on start and on window.resize */
+  addPaginationListener() {
     if (!this.isValid(this.eventListener)) this.eventListener = [];
     this.eventListener.push({
       element: window,
@@ -63,7 +70,8 @@ module.exports = class AbstractPage {
     });
   }
 
-  changeToPage (event) {
+  // changeToPage: changes pages (next, previous, or to specific page based on clicked event target)
+  changeToPage(event) {
     if (event?.currentTarget?.dataset?.page) this.currentPage = parseInt(event.currentTarget.dataset.page);
     else if (event.currentTarget.classList.value.indexOf('nextPage') !== -1) {
       if (this.currentPage + 1 <= this.pages[this.pages.length - 1]) this.currentPage++;
@@ -75,7 +83,8 @@ module.exports = class AbstractPage {
     this.router.renderPage({ animation: false });
   }
 
-  calculateElementsPerPage () {
+  // calculateElementsPerPage: calculates the amount of elements shown on one page
+  calculateElementsPerPage() {
     const oldval = this.elementsPerPage;
     const height = window.innerHeight;
     const heightForGrid = height - this.offset;
@@ -86,7 +95,8 @@ module.exports = class AbstractPage {
     if (this.router) this.router.renderPage({ animation: false });
   }
 
-  paginate (dbObject) {
+  // paginate: calculates and returns the shown slice of an object-array based on the pagination attributes
+  paginate(dbObject) {
     const start = ((this.currentPage - 1) * (this.elementsPerPage));
     const end = (this.currentPage * this.elementsPerPage < dbObject.length) ? (this.currentPage * this.elementsPerPage) : (dbObject.length);
     const displayedObjects = dbObject.slice(start, end);
@@ -95,47 +105,40 @@ module.exports = class AbstractPage {
     return displayedObjects;
   }
 
-  saveForm () {
-    if (this.isValid(this.getFormValues(this.formid))) {
-      this.form = this.getFormValues(this.formid);
-    }
-  }
+  // ----------------------------------------------
 
-  // render: should return pages rendered HTML as String
-  // async to be able to await data from server
-  async render () { return ''; }
+  /*
+   * RENDER FUNCTIONS
+   */
 
-  // registerClickHandler: finds element based on querySelector and adds a callback on click to it
-  registerClickHandler (querySelector, callback) {
-    const elements = document.querySelectorAll(querySelector);
-    for (const element of elements) {
-      if (element) {
-        element.addEventListener('click', function (event) {
-          callback(event);
-        }, true);
-      }
-    }
-  }
+  /* render: should return pages rendered HTML as String
+     async to be able to await data from server */
+  async render() { return ''; }
+
+
 
   // renderHandleBars: compiles handlebar template and injects data into it
-  renderHandleBars (templateSource, data) {
+  renderHandleBars(templateSource, data) {
     const template = Handlebars.compile(templateSource);
     return template(data);
   }
 
+  // ----------------------------------------------
+
+  /*
+   * FORM FUNCTIONS
+   */
+
   // getValue: gets value from element matched by a given querySelector
-  getValue (querySelector) {
+  getValue(querySelector) {
     return (document.querySelector(querySelector)?.value || null);
   }
-
-  // ----------------------------------------------
 
   /*  getFormValues (querySelector):
       Gets all "input", "textarea", "select", etc. - elements within a form (fetched by the querySelector)
       and returns their values in an object like: { name: element.value }
       where name (the key) is represented by the elements name attribute */
-
-  getFormValues (querySelector) {
+  getFormValues(querySelector) {
     const selector = `${querySelector} input, ${querySelector} select, ${querySelector} textarea`;
     const elements = document.querySelectorAll(selector);
     const result = {};
@@ -145,39 +148,17 @@ module.exports = class AbstractPage {
     return result;
   }
 
-  /* printContainer: Prints a given container-query-selector */
-
-  printContainer (querySelector, title, width, height) {
-    console.log('PRINT');
-    const reservationConfirmation = document.querySelector(querySelector);
-    const printWindow = window.open('', 'PRINT', `height=${width},width=${height}`);
-    const printHTML = `<html>
-      <head>
-        <title>${title}</title>
-      </head>
-      <body>
-        ${reservationConfirmation.innerHTML}
-      </body>
-    </html>`;
-
-    printWindow.document.write(printHTML);
-    printWindow.document.close(); // necessary for IE >= 10
-    printWindow.focus(); // necessary for IE >= 10*/
-    printWindow.print();
-    printWindow.close();
-
-    return true;
-  }
+  // ----------------------------------------------
 
   /*
-   * Axios Functions
+   * AXIOS FUNCTIONS
    */
 
   /*  async getData (urlpath):
       import axios
       wrap axios get call and return it to await it in the render function
       call: getData('/cinemas') */
-  async getData (urlpath) {
+  async getData(urlpath) {
     try {
       const response = await axios.get(this.apiURL + urlpath);
       return response?.data || null;
@@ -192,7 +173,7 @@ module.exports = class AbstractPage {
       call: postData('/cinemas', {name: "Kinosaal XY", seatRows: 10, seatsPerRow: 12});
       */
 
-  async postData (urlpath, data) {
+  async postData(urlpath, data) {
     try {
       const response = await axios.post(this.apiURL + urlpath, data);
       return response;
@@ -202,53 +183,86 @@ module.exports = class AbstractPage {
     }
   }
 
-  /* Pagination functions */
+  // ----------------------------------------------
 
   /*
-   * UIKit Functions
+   * UIKIT FUNCTIONS
    */
 
-  toast (message, status) {
+  // toast: displays UI-Kit Notification with given message and status
+  toast(message, status) {
     UIkit.notification(message, status || 'primary');
   }
 
-  openModal (selector) {
+  // openModal: opens an existing uk-modal based on a selector
+  openModal(selector) {
     console.log(UIkit.modal);
     const modal = UIkit.modal(selector);
     if (modal) modal.show();
   }
 
+  // ----------------------------------------------
+
   /*
    * HELPER FUNCTIONS
    */
 
-  /* isValid: checks if a value is not undefined or null */
-  isValid (value) {
+  // isValid: checks if a value is not undefined or null
+  isValid(value) {
     return value !== undefined && value !== null;
   }
 
-  /* isFilled: checks if a value is valid and not empty */
-  isFilled (value) {
+  // isFilled: checks if a value is valid and not empty
+  isFilled(value) {
     return this.isValid(value) && value !== '';
   }
 
-  /* formatDate: formats a datestring into german date time format */
-  formatDate (dateString) {
+  // formatDate: formats a datestring into german date time format
+  formatDate(dateString) {
     const date = new Date(dateString);
     const options = { year: 'numeric', month: '2-digit', day: 'numeric', hour: '2-digit', minute: '2-digit' };
     return date.toLocaleDateString('de-DE', options);
   }
 
-  /* clone: clones JSON compatible structures */
-  clone (value) {
-    if (value.length === undefined && typeof value === 'object') return Object.create(value);
-    if (value.length !== undefined && typeof value[0] === 'object') {
-      const returnArr = [];
-      for (const val of value) {
-        returnArr.push(Object.create(val));
+  // registerClickHandler: finds element based on querySelector and adds a callback on click to it
+  registerClickHandler(querySelector, callback) {
+    const elements = document.querySelectorAll(querySelector);
+    for (const element of elements) {
+      if (element) {
+        element.addEventListener('click', function (event) {
+          callback(event);
+        }, true);
       }
-      return returnArr;
     }
-    return JSON.parse(JSON.stringify(value));
+  }
+
+  // saveForm: saves form to get re-inserted into template when re-rendering
+  saveForm() {
+    if (this.isValid(this.getFormValues(this.formid))) {
+      this.form = this.getFormValues(this.formid);
+    }
+  }
+
+  // printContainer: Prints a given container-query-selector 
+  printContainer(querySelector, title, width, height) {
+    console.log('PRINT');
+    const reservationConfirmation = document.querySelector(querySelector);
+    const printWindow = window.open('', 'PRINT', `height=${width},width=${height}`);
+    const printHTML = `<html>
+        <head>
+          <title>${title}</title>
+        </head>
+        <body>
+          ${reservationConfirmation.innerHTML}
+        </body>
+      </html>`;
+
+    printWindow.document.write(printHTML);
+    printWindow.document.close(); // necessary for IE >= 10
+    printWindow.focus(); // necessary for IE >= 10*/
+    printWindow.print();
+    printWindow.close();
+
+    return true;
   }
 };
